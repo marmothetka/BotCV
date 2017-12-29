756a04787d70df6f882af53759d06e6e2d1e3151
marmothetka <golenok-ea5@narod.ru>
2017-12-29 14:40:28 +0300

    fix name of sln publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,3 @@
 nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=botcv-Web-Deploy.pubxml -p:Password=dllhKaMpkMK8PsvrKxWfpyzEzgtsyodPxSekx5Y2YCKTrnSi3Yu0Yb0gJ94t
+msbuild BotCV.sln -p:DeployOnBuild=true -p:PublishProfile=botcv-Web-Deploy.pubxml -p:Password=dllhKaMpkMK8PsvrKxWfpyzEzgtsyodPxSekx5Y2YCKTrnSi3Yu0Yb0gJ94t
 
